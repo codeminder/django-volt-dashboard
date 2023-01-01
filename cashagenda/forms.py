96ace4581fe51b7d7c38cc5327f8e094f1f03fc7
Roman <roman@example.com>
2023-01-01 18:50:04 +0200

    css volt  cashagenda/forms.py 	css volt  cashagenda/templates/cashagenda/add_cost.html

--- cashagenda/forms.py
+++ cashagenda/forms.py
@@ -6,3 +6,27 @@ class AddCost(forms.ModelForm):
     class Meta:
         model = Cost
         fields = '__all__'
+        widgets = {
+            'date': forms.DateTimeInput(attrs={
+                'class': 'form-control datepicker-input', 
+                'placeholder': 'YYYY-MM-DD hh:mm:ss',
+                'required': 'required'}),
+            'account': forms.Select(attrs={
+                'class': 'form-select', 
+                'required': 'required'}),
+            'sum': forms.NumberInput({
+                'class': 'form-control', 
+                'required': 'required',
+                'step': '0.01'}), 
+            'currency': forms.Select(attrs={
+                'class': 'form-select', 
+                'required': 'required'}),
+            'comment': forms.Textarea({
+                'class': 'form-control'
+                }),
+            'budget': forms.Select(attrs={
+                'class': 'form-select', 
+                'required': 'required'}),
+            'photo': forms.FileInput(attrs={
+                'class': 'form-control'}),
+        }
